6f75d509fe5bef68320a67027323d7b584d31e99
kimchi96 <tkchi141@gmail.com>
2020-07-15 16:19:29 +0700

    002

--- app/Http/Controllers/MyController.php
+++ app/Http/Controllers/MyController.php
@@ -222,10 +222,10 @@ class MyController extends Controller
         $id_token = JWTAuth::encode($payload, $keyGenerateToken)->get();*/
 
         // Create the token header
-        $header = [
+        $header = array(
             'typ' => 'JWT',
             'alg' => 'HS256'
-        ];
+        );
         dd($header);
         $base64url_Header = rtrim(strtr(base64_encode($header), '+/', '-_'), '='); 
 
