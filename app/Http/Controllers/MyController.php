a5155c6615e0ff3b4614176b91af1c012c9a5b1d
kimchi96 <tkchi141@gmail.com>
2020-07-15 15:58:43 +0700

    002

--- app/Http/Controllers/MyController.php
+++ app/Http/Controllers/MyController.php
@@ -226,7 +226,7 @@ class MyController extends Controller
             'typ' => 'JWT',
             'alg' => 'HS256'
         ];
-        $base64url_Header = rtrim(strtr(base64_encode($header[0]), '+/', '-_'), '='); 
+        $base64url_Header = rtrim(strtr(base64_encode($header[]), '+/', '-_'), '='); 
 
         // Create the token payload
         $user = MyUsersModel::where(['user_id' => $CodeModel->user_id])->first();
